38cf7fd09edc62e6cf797ef8ab11f00c8c8ba01d
Jenny Munoz <jen.munnoz@gmail.com>
2026-03-06 10:08:58 -0800

    Checking species availability

--- 3_Matching_NAWCA_ebird_ACAD.R
+++ 3_Matching_NAWCA_ebird_ACAD.R
@@ -1,20 +1,21 @@
-###_###_####_###_###_###_###_###_###_###_###_###_###_###_###_###_###_ _###_###_###_###_###_###_###_###_ 
-## checking for data availability for teh species of interest
-## ##
-## Objective: To check of the species of interest, for which ones we have ebird data 
-## This includes making sure that the taxonomy is properly matched 
-## 
-## 1) Check the access code to the ebird data  
-## 2) Check the ebird list of species
-## 3) 
-## 4) 
-## i) 
-## ii) 
-## iii) 
+###_###_####_###_###_###_###_###_###_###_###_###_###_###_###_###_###_
+### CHECKING DATA AVAILABILITY FOR SPECIES OF INTEREST
+###_###_####_###_###_###_###_###_###_###_###_###_###_###_###_###_###_
 ##
-## Updated and annotated by Jenny Munoz
+## Objective:
+## Check for which species of interest we have eBird Status & Trends data.
+## This includes verifying that taxonomy between datasets is properly matched.
+##
+## Workflow:
+## 1) Check the access code to the eBird data
+## 2) Retrieve the eBird list of species available in ebirdst
+## 3) Compare with the NAWCA species list
+## 4) Identify missing species and investigate taxonomy mismatches
+##
+## Updated and annotated by: Jenny Munoz
 ## Last updated: February 2026
-###_###_####_###_###_###_###_###_###_###_###_###_###_###_###_###_###__###_###_###_###_###_###_###_###_ 
+###
+###_###_####_###_###_###_###_###_###_###_###_###_###_###_###_###_###_
 
 # ================================================================
 # 0) SETUP
@@ -39,75 +40,105 @@ library(rmapshaper)
 library(terra)
 library(ggplot2)
 
-# ================================
-# 1)  DATA 
-# ================================
+# ================================================================
+# 1) DATA
+# ================================================================
 
-# The ebird data-species list 
+# eBird Status & Trends species list
 ebirdst::ebirdst_runs
 
-nawca_list<- read.csv("data/nawca_acad_species_match.csv", stringsAsFactors = FALSE) %>%
-  as_tibble() %>% 
-  mutate(common_name=NAWCA_species) %>% 
-  filter(ACAD=="Yes")
+# NAWCA species list matched with ACAD
+nawca_list <- read.csv(
+  "data/nawca_acad_species_match.csv",
+  stringsAsFactors = FALSE
+) %>%
+  as_tibble() %>%
+  mutate(common_name = NAWCA_species) %>%
+  filter(ACAD == "Yes")
 
+# Check unique ACAD values
 unique(nawca_list$ACAD)
 
-# ================================
-# 2) See FOR WHICH SPECIES WE HAVE EBIRD DATA 
-# ================================
+# ================================================================
+# 2) CHECK FOR WHICH SPECIES WE HAVE EBIRD DATA
+# ================================================================
 
-ebirdst_runs_selected<-ebirdst_runs %>% 
-  dplyr::select (species_code,scientific_name,common_name, breeding_start, breeding_end, breeding_quality)
-#View(ebirdst_runs_selected) # the overall list of ebird species 
+# Select key fields from eBird runs table
+ebirdst_runs_selected <- ebirdst_runs %>%
+  dplyr::select(
+    species_code,
+    scientific_name,
+    common_name,
+    breeding_start,
+    breeding_end,
+    breeding_quality
+  )
 
+# View full list if needed
+# View(ebirdst_runs_selected)
 
-# list of Bc birds
-nawca_species<-nawca_list %>% dplyr::select(common_name,global_or_american_predominantly, ACAD, PIF )
+# ================================================================
+# 3) MATCH NAWCA SPECIES WITH EBIRD DATA
+# ================================================================
 
+# List of NAWCA species
+nawca_species <- nawca_list %>%
+  dplyr::select(
+    common_name,
+    global_or_american_predominantly,
+    ACAD,
+    PIF
+  )
+
+# Match NAWCA species to eBird data
 match_nawca_species_ebird <- nawca_species %>%
   left_join(ebirdst_runs_selected, by = "common_name")
-View(match_nawca_species_ebird  )
 
-# # for which species we dont have it
+View(match_nawca_species_ebird)
+
+# ================================================================
+# 4) IDENTIFY SPECIES WITHOUT EBIRD DATA
+# ================================================================
+
 missing_species_nawca_ebird <- match_nawca_species_ebird %>%
   filter(is.na(species_code)) %>%
   dplyr::select(common_name, scientific_name) %>%
-  mutate(ebird_data="no")
-
-
-# --#---#-------------------------------------------
-
-# This is the list of species for which we dont have the data 
-
-#  1 Ancient Murrelet          NA              no        
-# 2 Black-crowned Night-Heron NA              no        
-# 3 Cassin's Auklet           NA              no        
-#  4 Common Murre              NA              no        
-#  5 Hudsonian Whimbrel        NA              no        
-#  6 Masked Duck               NA              no        
-#  7 Northern Goshawk          NA              no        
-#  8 Red-necked Phalarope      NA              no        
-#  9 White-rumped Sandpiper    NA              no        
-# 10 Yellow Rail               NA              no        
-# > 
-
-# Check manually for taxonomy or other causes of mistmach
-
- View(ebirdst::ebirdst_runs)
-
-#Ancient Murrelet # no data 
-#Black-crowned Night-Heron # difference in name, i just remove the "-" from Night-Heron from the NAWCA list manually 
-#Cassin's Auklet  # no data 
-#Common Murre # no data 
-#   Hudsonian Whimbrel           
-#   Masked Duck    
-# Northern Goshawk  
-# Red-necked Phalarope 
-#White-rumped Sandpiper
-#Yellow Rail     
+  mutate(ebird_data = "no")
 
 # ------------------------------------------------
+# List of species without eBird Status & Trends data
+# ------------------------------------------------
+#
+# 1  Ancient Murrelet
+# 2  Black-crowned Night-Heron
+# 3  Cassin's Auklet
+# 4  Common Murre
+# 5  Hudsonian Whimbrel
+# 6  Masked Duck
+# 7  Northern Goshawk
+# 8  Red-necked Phalarope
+# 9  White-rumped Sandpiper
+# 10 Yellow Rail
+#
 
+# ================================================================
+# 5) MANUAL TAXONOMY CHECK
+# ================================================================
 
+# Inspect full eBird species table
+View(ebirdst::ebirdst_runs)
 
+# Notes on taxonomy or data availability:
+#
+# Ancient Murrelet            -> No data available
+# Black-crowned Night-Heron   -> Name mismatch (hyphen removed manually in NAWCA list)
+# Cassin's Auklet             -> No data available
+# Common Murre                -> No data available
+# Hudsonian Whimbrel          -> Needs verification
+# Masked Duck                 -> Needs verification
+# Northern Goshawk            -> Needs verification
+# Red-necked Phalarope        -> Needs verification
+# White-rumped Sandpiper      -> Needs verification
+# Yellow Rail                 -> Needs verification
+#
+# ------------------------------------------------
