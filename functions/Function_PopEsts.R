d222fb131a3054b54cbaaa90a68e4e935ab56090
Barry Robinson <barry.robinson@ec.gc.ca>
2026-04-21 10:32:53 -0600

    Moved script used for testing to a separate script

--- functions/Function_PopEsts.R
+++ functions/Function_PopEsts.R
@@ -11,38 +11,9 @@
 #polys = list of shapefiles (sf objects) within which population estimates are needed
 
 #Note that script 1_CreateLookupInfo.R is run first. This downloads and creates the appropriate look up tables and spatial layers to determine which data sets are available for given species and locations
+#All lookup tables and spatial layers will be uploaded to the GitHub repo, so this step can be skipped by general users.
 
-#create test polygon and species lists
-library(sf)
-library(dplyr)
-library(BAMexploreR)
-library(ebirdst)
-library(terra)
-library(osfr)
-library(purrr)
 
-#Import layers for testing
-st_layers("data/test/ATWR.gpkg")
-sand <- st_read(dsn = "data/test/SAND.gpkg", layer = "SAND_boundary")
-atwr <- st_read(dsn = "data/test/ATWR.gpkg", layer = "ATWR_boundary")
-polys <- list("SAND" = sand, "ATWR" = atwr)
-
-polysf <- st_read(dsn = "data/spatial/examplePolys2.shp")
-#break polys into a list
-polys <- split(polysf, seq_len(nrow(polysf)))
-names(polys) <- polysf$NAME_E
-
-#Jenny and Andrew's test polygon
-polys <- list("Columbia Wetland" = st_read("data/spatial/TestPolys/Columbia Wetland Corridor area FINAL.shp"))
-species <- list("Sora", "American Coot", "Pied-billed Grebe")
-#test random list of species from the NAWCA priority species list
-nawca_list <- read.csv("data/nawca_acad_species_match.csv", stringsAsFactors = FALSE)
-species <- nawca_list$NAWCA_species[sample(1:nrow(nawca_list), 3)]
-
-species <- list("Savannah Sparrow", "White-throated Sparrow", "Mallard")
-
-sp <- species[[2]]
-pol <- polys[[1]]
 # ------------------------------------------------------------
 # 1. List of required packages
 # ------------------------------------------------------------
@@ -483,23 +454,6 @@ popEsts <- function(species, polys) {
   rownames(results) <- NULL
   return(results)
 } #END POP EST FUNCTION
-
-test <- popEsts(species, polys)
-
-final <- do.call(rbind, test)
-
-rownames(final) <- NULL
-
-
-#testing Jenny's function
-JennyGrebe <- estimate_pop_conservation_area_breeding("Pied-billed Grebe", polys[[1]], ACAD_clean)
-JennySora <- estimate_pop_conservation_area_breeding("Sora", polys[[1]], ACAD_clean)
-JennyCoot <- estimate_pop_conservation_area_breeding("American Coot", polys[[1]], ACAD_clean)
-jenny <- rbind(JennySora, JennyCoot, JennyGrebe)
-testJennynonBreed <- estimate_pop_conservation_area_global("Pied-billed Grebe", polys[[1]], ACAD_clean)
-
-
-
 
 
 
