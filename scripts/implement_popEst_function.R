af352c01fdf0e87f0963730196c80373ee82da3c
Barry Robinson <barry.robinson@ec.gc.ca>
2026-04-22 11:30:16 -0600

    Cleaned up example implementation script

--- scripts/implement_popEst_function.R
+++ scripts/implement_popEst_function.R
@@ -1,42 +1,87 @@
-#create test polygon and species lists
+##########################################################################################################
+#This script provides an example implementation of the Population Estimation function (Function_PopEsts.R)
+#Written by: Jenny Munoz and Barry Robinson
+#Last updated: April 22, 2026.
+##########################################################################################################
+
+#########################################################################################################
+# 1). Install any required packages that you don't already have (no need to load all of them)
+required_pkgs <- c("sf", "dplyr", "BAMexploreR", "ebirdst", "terra", "osfr", "purrr")
+missing <- required_pkgs[!vapply(required_pkgs, requireNamespace, logical(1), quietly = TRUE)]
+
+if (length(missing) > 0) {
+  install.packages(missing)
+}
+#########################################################################################################
+
+
+
+#########################################################################################################
+# 2). Set up files and access key needed for function
 library(sf)
-# library(dplyr)
-# library(BAMexploreR)
-# library(ebirdst)
-# library(terra)
-# library(osfr)
-# library(purrr)
+library(ebirdst)
+
+# eBird Status & Trends access key
+# An access key is required to download eBird Status & Trends data.
+#
+# Request or view your key here:
+#    https://ebird.org/st/request
+#
+# Save your access key as a text file someone on your computer
 
 
-#Import layers for testing
-st_layers("data/test/ATWR.gpkg")
-sand <- st_read(dsn = "data/test/SAND.gpkg", layer = "SAND_boundary")
-atwr <- st_read(dsn = "data/test/ATWR.gpkg", layer = "ATWR_boundary")
-polys <- list("SAND" = sand, "ATWR" = atwr)
+#set your ebird access key. You should only have to run this once. 
+set_ebirdst_access_key("************") # REPLACE WITH YOUR 12 DIGIT ALPHANUMERIC EBIRF ACCESS KEY
 
-polysf <- st_read(dsn = "data/spatial/examplePolys2.shp")
-#break polys into a list
-polys <- split(polysf, seq_len(nrow(polysf)))
-names(polys) <- polysf$NAME_E
+#Create folder to contain spatial data used for the analysis, 
+#included species distribution models and polygons for proposed NAWCA projects
+#This will create the following three nested folders within the root directory where
+#JV_abundance_estimates.Rproj was saved and loaded from.
+dir.create("data/spatial/polygons", recursive = TRUE) #only need to run this one. 
 
-#Jenny and Andrew's test polygon
-polys <- list("Columbia Wetland" = st_read("data/spatial/TestPolys/Columbia Wetland Corridor area FINAL.shp"))
-species <- list("Sora", "American Coot", "Pied-billed Grebe")
-#test random list of species from the NAWCA priority species list
-nawca_list <- read.csv("data/nawca_acad_species_match.csv", stringsAsFactors = FALSE)
-species <- nawca_list$NAWCA_species[sample(1:nrow(nawca_list), 3)]
 
+#save all polygons in "data/spatial/polygons". Polygons can be saved in any format accepted by
+#the sf package in R (e.g. .shp, .gpkg, .gbd)
+#Then load them into R as a list of individual polygons
+#ITS IMPORTANT THAT ALL THE POLYGONS ARE INPUT INTO A NAMED LIST
+#OUTPUT FROM THE POPULATION ESTIMATION FUNCTION WILL SUMMARIZE RESULTS
+#BASED ON THE NAMES YOU APPLY TO YOUR LIST OF POLYGONS
+
+# a) Example using .gpkg file
+sf::st_layers("data/spatial/polygons/ATWR.gpkg") #list layers within the gpkg file
+sf::st_layers("data/spatial/polygons/SAND.gpkg") #list layers within the gpkg file
+sand <- sf::st_read(dsn = "data/spatial/polygons/SAND.gpkg", layer = "SAND_boundary")
+atwr <- sf::st_read(dsn = "data/spatial/polygons/ATWR.gpkg", layer = "ATWR_boundary")
+polys <- list("SAND" = sand, "ATWR" = atwr) #creates a named list of polygons
+
+# b) Example using a single shapefile that contains polygons for multiple sites
+polysf <- sf::st_read(dsn = "data/spatial/polygons/examplePolys.shp") 
+polys <- split(polysf, seq_len(nrow(polysf))) #split individual polygon into a list
+names(polys) <- polysf$NAME_E #assign names to the list of polygons based on the "NAME_E" column from the attribute table
+
+# c) Example using multiple shapefiles
+poly1 <- sf::st_read("data/spatial/polygons/examplePoly1.shp")
+poly2 <- sf::st_read("data/spatial/polygons/examplePoly2.shp")
+polys <- list("poly1" = poly1, "poly2" = poly2)
+
+
+#Create a list of species using common names
+# a) Example using a custom list
+species <- c("Mallard", "Baird's Sparrow", "Common Yellowthroat")
+
+# b) Example pulling species from NAWCA Priority Species list (table included in R project)
+priority_spp <- read.csv("LookupData/nawca_acad_species_match.csv")
+priority_spp$NAWCA_species #View list of all priority species
+species <- c("Connecticut Warbler", "Baird's Sandpiper", "Lesser Yellowlegs") #copy and paste desired species into this list
+
+#Note that you could provide the entire priority species list to the popEst function, but this would
+#take a VERY long time to run and its is NOT recommended.
+#Its best to include smaller groups of species in separate runs of the function
+
+#######################################################################################################
+#3. Implement population estimation function for the list of polygons and species
+#Note this can take a while to run, especially if your polygon and/or species list is long
 source("functions/Function_PopEsts.R")
-test <- popEsts(species, polys)
+pop_ests <- popEsts(species, polys)
+#######################################################################################################
 
-
-
-
-
-
-#testing Jenny's function
-JennyGrebe <- estimate_pop_conservation_area_breeding("Pied-billed Grebe", polys[[1]], ACAD_clean)
-JennySora <- estimate_pop_conservation_area_breeding("Sora", polys[[1]], ACAD_clean)
-JennyCoot <- estimate_pop_conservation_area_breeding("American Coot", polys[[1]], ACAD_clean)
-jenny <- rbind(JennySora, JennyCoot, JennyGrebe)
-testJennynonBreed <- estimate_pop_conservation_area_global("Pied-billed Grebe", polys[[1]], ACAD_clean)
